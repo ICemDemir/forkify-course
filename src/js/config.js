cbcc22e23383b59a911584b4163a7257ccece904
ICemDemir <ismailcemdemir@gmail.com>
2024-01-04 14:40:56 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '02d9ffc7-b980-4dbf-bfa0-e41a572abc83';
+export const MODAL_CLOSE_SEC = 2.5;
